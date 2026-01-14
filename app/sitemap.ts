ad0ae6986039e93838de74fb566133052b66c33d
Pahri <ftyvfyy@gmail.com>
2026-01-14 08:37:51 +0700

    ubah site

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
@@ -7,12 +9,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1.0,
-    },
-    {
-      url: 'https://mfchmufid.my.id/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
     },
   ]
 }
